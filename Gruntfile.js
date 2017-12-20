20b4f3353aa7d2c648b26a19fe013c6f5e507ccf
Lediet <n_lediet@tss-hr420-dm05.fc.ca>
2017-12-20 16:23:15 -0500

    added sass .....

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,14 +16,33 @@ module.exports = function(grunt) {
                 dest : "dist/bundle.min.js"
             }
         },
+        sass : {
+            dist : {
+                options : {
+                    style: "compressed",
+                },
+                files : {
+                    "dist/bundle.min.css" : "src/sass/main.sass"
+                }
+            }
+        },
         watch : {
-            files : ["src/js/**/*.js"],
-            tasks : ["default"]
+            scripts : {
+                files : ["src/js/**/*.js"],
+                tasks : ["default"]
+            },
+            sass : {
+                files : ["src/sass/**/*.sass"],
+                tasks : ["sass"]
+            }
         }
     })
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch')
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat')
-    grunt.registerTask("default", ["concat", "uglify"])
+    grunt.registerTask("default", ["concat", "uglify", "sass"])
+    grunt.registerTask("watchFiles", ["watch", "sass"])
 }
 
